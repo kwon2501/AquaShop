df524214fc561ad9ff1c7137fe0c98c08f4b5060
kwon2501 <kwonkyulin25@gmail.com>
2024-12-17 21:28:52 +0900

    Initial commit

---
+++ src/main/java/com/example/aqua/AquaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.aqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaShopApplication.class, args);
+	}
+
+}
